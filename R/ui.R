c0df2644c06b7824688f4b3d16e2133031352384
z7H4AabqdYVj <jobrenne@outlook.com>
2016-01-27 15:45:39 +0100

    update station list, alphabethic

--- R/ui.R
+++ R/ui.R
@@ -15,10 +15,10 @@ ui <- fluidPage(
                   choices = list("ALL","5","20","40")),
       
       selectInput("Station", label = h4("station"), 
-                  choices = list("ALL","stpauls","eppanberg","kaltern","I1","I3","P1","P2","P3","B1","B2",
-                                 "B3","M1","M4","M5","M6","M7","S2","S4","S5","lana6","nals","terlanalt","terlanneu",
-                                 "M3","XS1","unterrain","gries","girlan","tramin13","neumarkt",
-                                 "latsch1","latsch3","latsch4","nemef1500","domef1500","vimef2000","vimes2000")),
+                  choices = list("ALL","B1","B2","B3","domef1500","domes1500","eppanberg","girlan","gries","I1","I3",
+                                 "kaltern","lana6","latsch1","latsch3","latsch4","M1","M3","M4","M5","M6",
+                                 "M7","nals","nemef1500","nemes1500","neumarkt","P1","P2","P3","S2","S4",
+                                 "S5","stpauls","terlanalt","terlanneu","tramin13","unterrain","vimef2000","vimes2000","XS1")),
       
       selectInput("Date", label = h4("date"), 
                   choices = list("ALL","2013","2014","2015")),
