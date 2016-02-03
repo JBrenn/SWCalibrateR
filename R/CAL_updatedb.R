3f6d8d7c9a197f4dc2425f80890a8c38f74221eb
z7H4AabqdYVj <jobrenne@outlook.com>
2016-02-03 17:08:07 +0100

    connection to db

--- R/CAL_updatedb.R
+++ R/CAL_updatedb.R
@@ -9,10 +9,9 @@ CAL_updatedb <- function(stations, inGIT="/home/jbr/GitHub/SMCcalibration/data/"
   # connect to db in data folder of project
   if (is.null(inGIT)) {
     pkg_path <- path.package("SMCcalibration")
-    setwd(file.path(pkg_path,"data"))
-    db = dbConnect(SQLite(), dbname="swc.sqlite")
+    db = dbConnect(SQLite(), dbname=file.path(pkg_path,"data","swc.sqlite")
   } else {
-    db = dbConnect(SQLite(), dbname="swc.sqlite")
+    db = dbConnect(SQLite(), dbname=file.path(inGIT,"swc.sqlite"))
   }
 
   
