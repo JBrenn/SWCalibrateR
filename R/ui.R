0dbe5e6c979dd45f160beb501a5f1eb7373d104a
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-09 12:04:27 +0200

    Update ui.R

--- R/ui.R
+++ R/ui.R
@@ -1,7 +1,8 @@
 if (!require("ggplot2")) install.packages("ggplot2")
 if (!require("Cairo")) install.packages("Cairo")
 if (!require("robustbase")) install.packages("robustbase")
-if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("tidyr")) install.packages("tidyr")
 
 data_def<-read.csv("SensorVSample_new.csv",sep=",",dec=".")
 
