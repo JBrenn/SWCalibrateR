6293ad2a5cdfcbddbd3575c2c7e94c7d22ecd96b
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-09 12:45:52 +0200

    Update lm_eq.R

--- R/lm_eq.R
+++ R/lm_eq.R
@@ -21,6 +21,6 @@ lm_eqn <- function(df, method){
     r2 = format(summary(m)$r.squared, digits = 3)))
   }
   
-  return(as.character(as.expression(eq)))   
+  return(as.expression(eq)) # as.character() 
   
 }
