d6bddab2487a02b3885db5b13b1553a38cc38e7d
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-09 12:29:33 +0200

    Update lm_eq.R

--- R/lm_eq.R
+++ R/lm_eq.R
@@ -11,14 +11,14 @@ lm_eqn <- function(df, method){
   
   if (coef(m)[2] < 0) {
     eq <- substitute(italic(y) == a  -  b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                     list(a = format(coef(m))[1]), digits = 2), 
-                          b = format(abs(coef(m))[2]), digits = 2), 
-                          r2 = format(summary(m)$r.squared, digits = 3)))
+                     list(a = format(coef(m)[1], digits = 2), 
+    b = format(abs(coef(m))[2], digits = 2), 
+r2 = format(summary(m)$r.squared, digits = 3)))
   } else {
     eq <- substitute(italic(y) == a  +  b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                     list(a = format(coef(as.numeric(m)))[1]), digits = 2), 
-                          b = format(abs(coef(as.numeric(m))))[2]), digits = 2), 
-                          r2 = format(summary(m)$r.squared, digits = 3)))
+                     list(a = format(coef(as.numeric(m))[1], digits = 2), 
+    b = format(abs(coef(as.numeric(m)))[2], digits = 2), 
+r2 = format(summary(m)$r.squared, digits = 3)))
   }
   
   return(as.character(as.expression(eq)))   
