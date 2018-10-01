51055ac7c39cf2785e77c245a3ad5f0e86f0daf6
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2018-10-01 10:38:10 +0200

    delete functins and documentations no longer needed

--- R/CAL_updateRData.R
+++
@@ -1,5 +0,0 @@
-CAL_updateRData <- function(path, filename = "SUMMARY_TOTAL.csv") 
-{
-  data <- read.csv(file.path(path, filename), header=TRUE)
-  save(list = "data", file = file.path(path,"SensorVSample.RData"))
-}
