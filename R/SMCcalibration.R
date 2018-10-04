e643517c4b5011bc327efd564ffbb837d201c128
JBrenn <johannes.brenner@ufz.de>
2018-10-04 14:45:43 +0200

    package info

---
+++ R/SMCcalibration.R
@@ -0,0 +1,11 @@
+#' SMCcalibration: A package for estimate and dignose soil moisture sensor
+#' claibration.
+#'
+#' The SMCcalibration package provides an interactive shiny framework.
+#'
+#' @section SMCcalibration functions:
+#' The SMCcalibration functions ...
+#'
+#' @docType package
+#' @name SMCcalibration
+NULL
