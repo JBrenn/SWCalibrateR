e16c600ceb08e84cf1895c6c4b7eae7633284d69
JBrenn <johannes.brenner@ufz.de>
2018-11-28 17:37:03 +0100

    add leading 0

--- data-raw/generalisedata.R
+++ data-raw/generalisedata.R
@@ -8,7 +8,7 @@ load("./data-raw/SensorVSample.RData")
 # levels
 numberlevels <- 1:length(levels(data$`Project ID`))
 # new names / levels
-newlevels <- paste("Project", numberlevels, sep=" ")
+newlevels <- paste("Project", stringr::str_pad(numberlevels, 2, pad = "0"), sep=" ")
 # add to current levels
 levels(data$`Project ID`) <- c( levels(data$`Project ID`), newlevels)
 # unique data frame subset entries
@@ -25,7 +25,7 @@ data$`Project ID` <- factor(data$`Project ID`)
 # levels
 numberlevels <- 1:length(levels(data$`Station ID`))
 # new names / levels
-newlevels <- paste("Station", numberlevels, sep=" ")
+newlevels <- paste("Station", stringr::str_pad(numberlevels, 2, pad = "0"), sep=" ")
 # add to current levels
 levels(data$`Station ID`) <- c( levels(data$`Station ID`), newlevels)
 # unique data frame subset entries
@@ -55,6 +55,25 @@ for (i in ii)
       size = 1, replace = TRUE)
 # reduce levels
 data$Landuse <- factor(data$Landuse)
+
+#4 generalise sensor name 
+# change "SensorMean\t" -> "SensorMean"
+data$`Sensor ID`[data$`Sensor ID`=="SensorMean\t"] <- "SensorMean"
+# levels
+numberlevels <- 1:length(levels(data$`Sensor ID`))
+# new names / levels
+newlevels <- newlevels <- paste("Sensor", stringr::str_pad(numberlevels, 2, pad = "0"), sep=" ")
+# add to current levels
+levels(data$`Sensor ID`) <- c( levels(data$`Sensor ID`), newlevels)
+# unique data frame subset entries
+uniquedata <- unique(data$`Sensor ID`)
+# run over # of unique enntries
+ii <- 1:length( uniquedata )
+# and change old to new levels / values
+for (i in ii)
+  data$`Sensor ID`[data$`Sensor ID` == uniquedata[i]] <- newlevels[i]
+# reduce levels
+data$`Sensor ID` <- factor(data$`Sensor ID`)
 
 # reduce data set to 200 entries
 data <- data[sample(x = 1:length(data$`Station ID`), 200), ]
