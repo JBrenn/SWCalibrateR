88cb3a2af3fca658dc7a346b17d95c23b25bd870
JBrenn <johannes.brenner@ufz.de>
2018-11-30 13:12:04 +0100

    new package doc

---
+++ R/SWCalibrateR.R
@@ -0,0 +1,7 @@
+#' SWCalibrateR: A package for estimate and diagnose calibration equations for soil moisture sensors
+#'
+#' The SWCalibrateR package provides an interactive shiny framework.
+#'
+#' @docType package
+#' @name SWCalibrateR
+NULL
