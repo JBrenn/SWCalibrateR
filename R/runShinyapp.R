5024993354f191a411339d2c600ac847c2ebb3c3
JBrenn <johannes.brenner@ufz.de>
2018-12-04 11:56:46 +0100

    new package name

--- R/runShinyapp.R
+++ R/runShinyapp.R
@@ -1,9 +1,8 @@
 #' @title run Shiny app for soil moisture sensor calibration
-#' @description \code{runShinyapp} does start shiny application for soil
-#' moisture sensor calibration. 
+#' @description \code{runShinyapp} does start shiny application for soil moisture sensor calibration. 
 #' @return shiny gui
 #' @examples 
-#' SMCcalibration::runShinyapp()
+#' SWCalibrateR::runShinyapp()
 #' @seealso 
 #'  \code{\link[shiny]{runApp}}
 #' @rdname runShinyapp
